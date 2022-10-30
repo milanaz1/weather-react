439b735392e6e5679aaa109e676adc1daa89f6c8
Milana <95094825+milanaz1@users.noreply.github.com>
2022-10-30 16:09:41 +0200

    HTML and CSS transferred from Codesandox

--- src/App.js
+++ src/App.js
@@ -1,19 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
-import Weather from './Weather';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div class="all-info">
+        <div class="info">
+          <h1>
+            <span id="city-now">Kyiv</span>
+            <span id="degrees">20 </span>{" "}
+            <span id="units">
+              {" "}
+              <a href="/" id="celsius" class="active">
+                °C
+              </a>{" "}
+              |{" "}
+              <a href="/" id="fahrenheit">
+                °F
+              </a>
+            </span>
+          </h1>
+          <h2>
+            Last updated: <span id="currentDate"></span>
+          </h2>
+          <div class="search">
+            <form class="city-form">
+              <input
+                id="search-text"
+                type="text"
+                placeholder="Choose the city"
+              />
+              <input type="submit" id="submit-button" />
+            </form>
+          </div>
+          <div class="container">
+            <div class="row cols">
+              <div class="col">
+                <h3 class="colName">Daily weather</h3>
+                <img
+                  src="https://openweathermap.org/img/wn/04n@2x.png"
+                  alt="weather-icon"
+                  id="weather-icon"
+                />
+                <div class="daily">
+                  <p id="weatherDescription"></p>
+                  <hr />
+                  <p>
+                    Wind speed: <span id="windSpeed"></span>km/h
+                  </p>
+                  <p>
+                    Humidity: <span id="humidity"></span>%
+                  </p>
+                </div>
+              </div>
+              <div class="col">
+                <h3 class="colName">Weekly weather</h3>
+                <div class="future-forecast" id="card-body-forecast"></div>
+              </div>
+            </div>
+          </div>
+        </div>
         <p>
-          Weather app
+          Coded by Milana Zadorozhna -
+          <a href="https://github.com/milanaz1/my-weather-project/tree/main/src">
+            GitHub
+          </a>
         </p>
-        <Weather city="Kyiv"/>
-      </header>
+      </div>
     </div>
   );
 }
 
-export default App;
