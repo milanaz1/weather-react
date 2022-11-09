425b04bddba29c452391e425bda237e9b38caeee
Milana <95094825+milanaz1@users.noreply.github.com>
2022-11-09 15:59:12 +0200

    Added bootstrap and info to proper files

--- src/App.js
+++ src/App.js
@@ -1,71 +1,10 @@
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
-      <div class="all-info">
-        <div class="info">
-          <h1>
-            <span id="city-now">Kyiv</span>
-            <span id="degrees">20 </span>{" "}
-            <span id="units">
-              {" "}
-              <a href="/" id="celsius" class="active">
-                °C
-              </a>{" "}
-              |{" "}
-              <a href="/" id="fahrenheit">
-                °F
-              </a>
-            </span>
-          </h1>
-          <h2>
-            Last updated: <span id="currentDate"></span>
-          </h2>
-          <div class="search">
-            <form class="city-form">
-              <input
-                id="search-text"
-                type="text"
-                placeholder="Choose the city"
-              />
-              <input type="submit" id="submit-button" />
-            </form>
-          </div>
-          <div class="container">
-            <div class="row cols">
-              <div class="col">
-                <h3 class="colName">Daily weather</h3>
-                <img
-                  src="https://openweathermap.org/img/wn/04n@2x.png"
-                  alt="weather-icon"
-                  id="weather-icon"
-                />
-                <div class="daily">
-                  <p id="weatherDescription"></p>
-                  <hr />
-                  <p>
-                    Wind speed: <span id="windSpeed"></span>km/h
-                  </p>
-                  <p>
-                    Humidity: <span id="humidity"></span>%
-                  </p>
-                </div>
-              </div>
-              <div class="col">
-                <h3 class="colName">Weekly weather</h3>
-                <div class="future-forecast" id="card-body-forecast"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <p>
-          Coded by Milana Zadorozhna -
-          <a href="https://github.com/milanaz1/weather-react" target="_blank" rel="noreferrer">
-            GitHub
-          </a>
-        </p>
-      </div>
+      <Weather city="Berlin"/>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,96 @@
+import React, { useState } from 'react';
 import axios from 'axios';
 
 export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({ready: false});
     function handleResponse(response) {
-        alert (`The weather in ${response.data.name} is ${response.data.main.temp}°C`);
+        console.log(response.data);
+        setWeatherData({
+            ready: true,
+            temperature: Math.round(response.data.main.temp),
+            imgUrl: '',
+            date: "Tuesday, 7:00",
+            description: response.data.weather[0].description,
+            wind: Math.round(response.data.wind.speed),
+            humidity: response.data.main.humidity,
+            city: response.data.name
+        });
     }
-    let api_key = `9b71ae985af1baa6a806c916c0a028f9`;
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${api_key}&units=metric`;
-    axios.get(url).then(handleResponse);
+
+    if (weatherData.ready) {
+        return (
+            <div className="all-info">
+            <div className="info">
+              <h1>
+                <span id="city-now"> {weatherData.city} </span>
+                <span id="degrees"> {weatherData.temperature} </span>{" "}
+                <span id="units">
+                  {" "}
+                  <a href="/" id="celsius" className="active">
+                    °C
+                  </a>{" "}
+                  |{" "}
+                  <a href="/" id="fahrenheit">
+                    °F
+                  </a>
+                </span>
+              </h1>
+              <p className="text-capitalize"> {weatherData.description} </p>
+              <h2>
+                Last updated: <span id="currentDate"> {weatherData.date} </span>
+              </h2>
+              <div className="search">
+                <form className="city-form">
+                  <input
+                    id="search-text"
+                    type="text"
+                    placeholder="Choose the city"
+                  />
+                  <input type="submit" id="submit-button" />
+                </form>
+              </div>
+              <div className="container">
+                <div className="row cols">
+                  <div className="col">
+                    <h3 className="colName">Daily weather</h3>
+                    <img
+                      src={weatherData.imgUrl}
+                      alt={weatherData.description}
+                      id="weather-icon"
+                    />
+                    <div className="daily">
+                      <p id="weatherDescription"></p>
+                      <hr />
+                      <p>
+                        Wind speed: <span id="windSpeed">{weatherData.wind}</span>km/h
+                      </p>
+                      <p>
+                        Humidity: <span id="humidity">{weatherData.humidity}</span>%
+                      </p>
+                    </div>
+                  </div>
+                  <div className="col">
+                    <h3 className="colName">Weekly weather</h3>
+                    <div className="future-forecast" id="card-body-forecast"></div>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+            <p>
+              Coded by Milana Zadorozhna -
+              <a href="https://github.com/milanaz1/weather-react" target="_blank" rel="noreferrer">
+                GitHub
+              </a>
+            </p>
+          </div>
+        );
+    } else {
+        const api_key = `9b71ae985af1baa6a806c916c0a028f9`;
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${api_key}&units=metric`;
+        axios.get(url).then(handleResponse);
+
+        return "Loading..";
+    }
+
 }
