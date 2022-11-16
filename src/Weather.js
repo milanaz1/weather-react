25d4e913d7e1dfda208043ec8bc5fc4db3ff64b4
Milana <95094825+milanaz1@users.noreply.github.com>
2022-11-16 17:50:52 +0200

    Add search engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import FormattedDate from './FormattedDate';
+import WeatherInfo from './WeatherInfo';
 
 export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({ ready: false });
+    const [city, setCity] = useState(props.city);
     function handleResponse(response) {
         console.log(response.data);
         setWeatherData({
@@ -18,9 +19,57 @@ export default function Weather(props) {
         });
     }
 
+function search() {
+  const api_key = `9b71ae985af1baa6a806c916c0a028f9`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
+  axios.get(url).then(handleResponse);
+}
+
+
+function handleSubmit(event) {
+      event.preventDefault();
+      search();
+    }
+
+function handleCityChange(event) {
+  setCity(event.target.value);
+}
+
     if (weatherData.ready) {
         return (
-            <div className="all-info">
+          <div className="all-info">
+                    <div className="info">
+              <div className="search">
+                <form onSubmit={handleSubmit} className="city-form">
+                  <input
+                    id="search-text"
+                    type="text"
+                    placeholder="Choose the city"
+                    onChange={handleCityChange}
+                  />
+                  <input type="submit" id="submit-button" />
+                </form>
+              </div>
+              <WeatherInfo data={weatherData}/>
+              </div>
+            <p>
+              Coded by Milana Zadorozhna -
+              <a href="https://github.com/milanaz1/weather-react" target="_blank" rel="noreferrer">
+                GitHub
+              </a>
+            </p>
+          </div>
+        );
+    } else {
+        search();
+        return "Loading..";
+    }
+
+}
+
+
+
+           {/* <div className="all-info">
             <div className="info">
               <h1>
                 <span id="city-now"> {weatherData.city} </span>
@@ -40,16 +89,9 @@ export default function Weather(props) {
               <div>
                 Last updated: <FormattedDate date={ weatherData.date }/>
               </div>
-              <div className="search">
-                <form className="city-form">
-                  <input
-                    id="search-text"
-                    type="text"
-                    placeholder="Choose the city"
-                  />
-                  <input type="submit" id="submit-button" />
-                </form>
-              </div>
+              
+*/}
+                           {/*  
               <div className="container">
                 <div className="row cols">
                   <div className="col">
@@ -78,20 +120,4 @@ export default function Weather(props) {
                 </div>
               </div>
             </div>
-            <p>
-              Coded by Milana Zadorozhna -
-              <a href="https://github.com/milanaz1/weather-react" target="_blank" rel="noreferrer">
-                GitHub
-              </a>
-            </p>
-          </div>
-        );
-    } else {
-        const api_key = `9b71ae985af1baa6a806c916c0a028f9`;
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${api_key}&units=metric`;
-        axios.get(url).then(handleResponse);
-
-        return "Loading..";
-    }
-
-}
+        */}
---
+++ src/WeatherInfo.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import FormattedDate from './FormattedDate';
+
+export default function weatherInfo(props) {
+    return (
+        <div>
+          <h1>
+            <span id="city-now"> {props.data.city} </span>
+            <span id="degrees"> {props.data.temperature} </span>{" "}
+            <span id="units">
+              {" "}
+              <a href="/" id="celsius" className="active">
+                °C
+              </a>{" "}
+              |{" "}
+              <a href="/" id="fahrenheit">
+                °F
+              </a>
+            </span>
+          </h1>
+          <p className="text-capitalize"> {props.data.description} </p>
+          <div>
+            Last updated: <FormattedDate date={props.data.date }/>
+          </div>
+          <div className="container">
+            <div className="row cols">
+              <div className="col">
+                <h3 className="colName">Daily weather</h3>
+                <img
+                  src={props.data.imgUrl}
+                  alt={props.data.description}
+                  id="weather-icon"
+                />
+                <div className="daily">
+                  <p id="weatherDescription"></p>
+                  <hr />
+                  <p>
+                    Wind speed: <span id="windSpeed">{props.data.wind}</span>km/h
+                  </p>
+                  <p>
+                    Humidity: <span id="humidity">{props.data.humidity}</span>%
+                  </p>
+                </div>
+              </div>
+              <div className="col">
+                <h3 className="colName">Weekly weather</h3>
+                <div className="future-forecast" id="card-body-forecast"></div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+        );
+}
